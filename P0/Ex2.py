0f0f7cb5957d17fb4c2af12569f56bcad78c5003
ClaireOnline <uniclara2020@gmail.com>
2021-02-24 18:05:51 +0100

    Organizing files in a different folder and referencing it with paths in the file Ex2.py

--- P0/Ex2.py
+++ P0/Ex2.py
@@ -1,6 +1,7 @@
 import P0.Seq0 as Seq0
 
+FOLDER = "./Sequences/"
 ID = "U5.txt"
 
-U5_Seq = Seq0.seq_read_fasta(ID)
+U5_Seq = Seq0.seq_read_fasta(FOLDER + ID)
 print("The first 20 bases are:", U5_Seq[0:19])
