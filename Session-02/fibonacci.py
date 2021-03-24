6e8a0e785ba49a503bc4c63b2c25d841a5bd1bce
ClaraOnline <uniclara2020@gmail.com>
2021-03-24 15:17:12 +0100

    session 2: debug and exercises (not finished)

---
+++ Session-02/fibonacci.py
@@ -0,0 +1,10 @@
+res_1 = 1
+res_2 = 1
+c = 0
+print(0, end=" ")
+while c <= 11:
+    print(res_1, end=" ")
+    c += 1
+    res_2 = res_1 + res_2
+    res_1 = res_2
+# finish session 2 exercises https://github.com/davidrol6/2020-2021-PNE/wiki/Tools-II
