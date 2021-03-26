3b88d31139035d13df44cd6dd9feb452bf5b1934
ClaraOnline <uniclara2020@gmail.com>
2021-03-26 20:53:36 +0100

    session 02 exercise 1 revised

--- Session-02/fibonacci.py
+++ Session-02/fibonacci.py
@@ -1,4 +1,20 @@
-res = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+# ONLY PRINT
+#    res = [0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
+#    for n in res:
+#       print(n, end=" ")
 
-for n in res:
-    print(n, end=" ")
+# CLASS CORRECTION
+#    series = [0, 1]
+#    a, b = 0, 1
+#    for n in range(1, 10):
+#        c = b + a
+#        series.append(c)
+#        a, b = b, c
+#    print(series)
+
+# MY CODE
+fib = [0, 1]
+for c in range(1, 10):
+    fn = fib[c] + fib[c-1]
+    fib.append(fn)
+print(fib)
