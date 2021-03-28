a864c9e32a9ce3e33f002bf7140d5684c8bbed2e
ClaraOnline <uniclara2020@gmail.com>
2021-03-28 19:34:04 +0200

    session 06 exercise 2 (class imported)

--- Session-06/Ex2.py
+++ Session-06/Ex2.py
@@ -1,26 +1,4 @@
-class Seq:
-    """A class for representing sequences"""
-    def __init__(self, strbases):
-        c = 0
-        for base in strbases:
-            if base != "A" and base != "C" and base != "T" and base != "G":
-                print("ERROR!!")
-                self.strbases = "ERROR"
-                break
-            else:
-                c += 1
-        if c == len(strbases):
-            self.strbases = strbases
-            print("New sequence created!")
-
-    def __str__(self):
-        """Method called when the object is being printed"""
-
-        return self.strbases
-
-    def len(self):
-        """Calculate the length of the sequence"""
-        return len(self.strbases)
+from seq01 import Seq
 
 
 def print_seqs(seqlist):
