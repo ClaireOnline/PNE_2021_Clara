1ba74829964d12a0e6c6e1cba083a5416c5d0df3
ClaraOnline <uniclara2020@gmail.com>
2021-03-28 20:08:53 +0200

    session 06 exercise 3 (seq01 modified not to print its main code)

---
+++ Session-06/Ex3.py
@@ -0,0 +1,23 @@
+from seq01 import Seq
+from Ex2 import print_seqs
+
+
+def generate_seqs(pattern, n):
+    seq_list = []
+    seq = ""
+    for c in range(0, n):
+        seq += pattern
+        seq_list.append(Seq(seq))
+    return seq_list
+
+
+# MAIN PROGRAM
+seq_list1 = generate_seqs("A", 3)
+seq_list2 = generate_seqs("AC", 5)
+
+print("List 1:")
+print_seqs(seq_list1)
+
+print()
+print("List 2:")
+print_seqs(seq_list2)
--- Session-06/seq01.py
+++ Session-06/seq01.py
@@ -37,12 +37,12 @@ class Gene(Seq):
 
 
 # --- Main program
-s1 = Seq("AGTACACTGGT")
-g = Gene("CGTAAC", "FRAT1")
+# s1 = Seq("AGTACACTGGT")
+# g = Gene("CGTAAC", "FRAT1")
 
 # -- Printing the objects
-print(f"Sequence 1: {s1}")
-print(f"Gene: {g}")
+# print(f"Sequence 1: {s1}")
+# print(f"Gene: {g}")
 
 #    s1 = Seq("ACCTGC")
 #    s2 = Seq("Hello? Am I a valid sequence?")
