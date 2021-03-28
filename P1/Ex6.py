3887926989c7a08f02c77be9ce2da69edd4ff168
ClaraOnline <uniclara2020@gmail.com>
2021-03-28 21:17:32 +0200

    practice 1 Corrected exercises' names (4, 5 and 6)

--- P1/Ex6.py
+++ P1/Ex6.py
@@ -10,5 +10,8 @@ s2 = Seq("ACTGA")
 s3 = Seq("Invalid sequence")
 
 print("Sequence 1: (Length: " + str(Seq.len(s1)) + ") ", s1)
+print("\tBases:", Seq.count(s1))
 print("Sequence 2: (Length: " + str(Seq.len(s2)) + ") ", s2)
+print("\tBases:", Seq.count(s2))
 print("Sequence 3: (Length: " + str(Seq.len(s3)) + ") ", s3)
+print("\tBases:", Seq.count(s3))
